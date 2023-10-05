f634f500f5df279829b23bb24fb9eb12e8bd6876
Chris Kirksey <chris.kirksey@aesbus.com>
2023-10-05 17:22:14 -0500

    dont specify port or address

--- ip-test/app.js
+++ ip-test/app.js
@@ -3,8 +3,9 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 
-// const server = app.listen(3000, '0.0.0.0', () => {
-const server = app.listen(3000, '127.0.0.1', () => {
+const server = app.listen(() => {
+    // const server = app.listen(3000, '0.0.0.0', () => {
+    // const server = app.listen(3000, '127.0.0.1', () => {
     // const server = app.listen(3000, '::', () => {
     // const server = app.listen(3000, '::1', () => {
     const address = server.address();
